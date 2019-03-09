f4b27643967b7c0a2de08336432c89abf91be7ff
Kamil Goś <kamil.gos00@gmail.com>
2019-03-09 12:59:17 +0100

    Added data from ta000 instance to dane.txt file.
    Download data to table in LAB1.py

--- LAB1.py
+++ LAB1.py
@@ -1,2 +1,30 @@
 #Algroytm Pzepływowy i Johnsona
+print("Starting program...")
+
+data_tab=[]
+
+# Wczytywanie danych
+file = open("dane.txt", "r")
+data = file.read()
+print("Reading data:")
+print(data)
+
+for number in data.split():         #Wczytywanie danych do tablicy
+    data_tab.append(int(number))
+
+print("Data transfered to table: ")
+print(data_tab)
+
+task_val = data_tab[0]
+machines_val = data_tab[1]
+
+print("Number of tasks: ", task_val)
+print("Numer of machines: ", machines_val)
+
+
+file.close()
+
+
+
+#input('Press ENTER to continue...')
 
