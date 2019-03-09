b93446b88b1973c68119acf1c12773a451e07c2d
Kamil Goś <kamil.gos00@gmail.com>
2019-03-09 14:25:43 +0100

    szkielet johnsona

--- LAB1.py
+++ LAB1.py
@@ -22,15 +22,25 @@ def read_data(filename):
     return tasks_val, machines_val, tasks
 
 
-read_data("data_2m.txt")
+def johnson_algorithm(machines_val, tasks):
+    if(machines_val == 2):
+        print("Two machines algorithm")
+        return johnson2m(tasks)
+    if (machines_val == 3):
+        print("Three machines algorithm")
+        return johnson3m(tasks)
 
 
+def johnson2m(tasks):
+    print("2m")
 
 
+def johnson3m(tasks):
+    print("3m")
 
 
-
-
+tasks_val, machines_val, tasks = read_data("data_2m.txt")
+johnson_algorithm(machines_val, tasks)
 
 
 #input('Press ENTER to continue...')
