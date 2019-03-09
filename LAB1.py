14ce153b76138ae70e0275d608e874c3cb62c8c2
Kamil Goś <kamil.gos00@gmail.com>
2019-03-09 16:51:48 +0100

    3 machines algorithm= DONE

--- LAB1.py
+++ LAB1.py
@@ -24,7 +24,7 @@ def read_data(filename):
 
 def johnson_algorithm(machines_val, tasks):
     if(machines_val == 2):
-        print("Two machines algorithm")
+        print("Two machines algorithm: START")
         return johnson2m(tasks)
     if (machines_val == 3):
         print("Three machines algorithm")
@@ -32,46 +32,57 @@ def johnson_algorithm(machines_val, tasks):
 
 
 def johnson2m(tasks):
-    #print("2m")
-    #print(len(tasks[1]))
     mt_row = 0
     mt_column = 0
     tab1 = []
     tab2 = []
     iter = 0
-    #print(len(tasks))
     while(iter != len(tasks)):
         min_time = 100
-       # print(tasks)
         for i in range(0, len(tasks)):
             for j in range(0, len(tasks[i])):
                 if(min_time > tasks[i][j]):
                     min_time = tasks[i][j]
                     mt_row = i
                     mt_column = j
-        #print("Iteracja:", iter, "MT_ROW:",mt_row, "MT_COL:", mt_column)
         tasks[mt_row] = 1000
 
         if (mt_column == 0):
             tab1.append(mt_row)
-           # print("Dodaje", mt_row, "Do tab 1")
         if (mt_column == 1):
             tab2.append(mt_row)
-           # print("Dodaje", mt_row, "Do tab 2")
-
         iter = iter +1
-        #print(iter)
 
-    print(tab1)
-    print(tab2)
     sequence = tab1 + list(reversed(tab2))
-    print("Sequence: ", sequence)
+    print("Best sequence: ", sequence)
+    print("Two machines algorithm: DONE")
+
+
+##Creating 2 virtual machines
+def transform3to2(tasks):
+    virtual = np.zeros((len(tasks),2))
+
+    for i in range(0, len(tasks)):
+        virtual[i][0] = tasks[i][0] + tasks[i][1]
+        virtual[i][1] = tasks[i][1] + tasks[i][2]
+
+    print("Virtual machines: \n", virtual)
+    return virtual
 
 def johnson3m(tasks):
-    print("3m")
+    print("Three machines algorithm: START")
+    virtual_machine = transform3to2(tasks)
+    johnson2m(virtual_machine)
+    print("Three machines algorithm: DONE")
 
 
-tasks_val, machines_val, tasks = read_data("data_2m.txt")
+
+
+
+
+
+##MAIN##
+tasks_val, machines_val, tasks = read_data("data_3m.txt")
 johnson_algorithm(machines_val, tasks)
 
 
