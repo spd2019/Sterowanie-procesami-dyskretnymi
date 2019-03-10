2418704a967e7ce8810c3faf61c5f69727af342c
Kamil Goś <kamil.gos00@gmail.com>
2019-03-11 00:23:07 +0100

    Final version (packed)

--- Zajecia_pierwsze/LAB1.py
+++ Zajecia_pierwsze/LAB1.py
@@ -8,9 +8,9 @@ print("Starting program...")
 
 ##MAIN##
 if(TWO_OR_THREE == 2):
-    tasks_val, machines_val, tasks = read_data.read_data("data_2m.txt")
+    tasks_val, machines_val, tasks = read_data.read_data("Zajecia_pierwsze\data_2m.txt")
 if(TWO_OR_THREE == 3):
-    tasks_val, machines_val, tasks = read_data.read_data("data_3m.txt")
+    tasks_val, machines_val, tasks = read_data.read_data("Zajecia_pierwsze\data_3m.txt")
     print("The package ta000 has been loaded")
 
 bruteforce.bruteforce(tasks, machines_val, tasks_val)
