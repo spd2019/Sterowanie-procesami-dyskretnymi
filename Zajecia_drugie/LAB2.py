1b5016203934639ca692d53bf688f52229ee040a
spd2019 <47981399+spd2019@users.noreply.github.com>
2019-03-16 15:57:26 +0100

    Update LAB2.py

--- Zajecia_drugie/LAB2.py
+++ Zajecia_drugie/LAB2.py
@@ -1,4 +1,6 @@
 print("Welcome to LAB2")
 from Zajecia_drugie import read_data, neh
 
-tasks_val, machines_val, tasks = read_data.read_data("ta001.txt")
+tasks_val, machines_val, tasks = read_data.read_data("Zajecia_drugie/ta001.txt")
+
+neh.neh(tasks, machines_val, tasks_val)
