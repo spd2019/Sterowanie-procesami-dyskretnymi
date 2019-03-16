2db4a9c3a1e37b4706e9a4e56364915a02a2950e
spd2019 <47981399+spd2019@users.noreply.github.com>
2019-03-16 15:50:55 +0100

    Create neh.py

---
+++ Zajecia_drugie/neh.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+def makespan(order, tasks, machines_val):
+    times = []
+    for i in range(0, machines_val):
+        times.append(0)
+    for j in order:
+        times[0] += tasks[j][0]
+        for k in range(1, machines_val):
+            if times[k] < times[k-1]:
+                times[k] = times[k-1]
+            times[k] += tasks[j][k]
+    return max(times)
+
+tasks_val, machines_val, tasks = read_data("data1.txt")
+
+def sum_and_order(tasks_val, machines_val, tasks):
+    tab = []
+    tab1 = []
+    for i in range(0, tasks_val):
+        tab.append(0)
+        tab1.append(0)
+    for j in range(0, tasks_val):        #sumuje czas wykonywania się poszczególnych zadań
+        for k in range(0, machines_val):
+            tab[j] += tasks[j][k]
+    #print(tab)
+    tmp_tab = tab.copy()
+    place = 0
+    iter = 0
+    while(iter != tasks_val):           #układa zadania w kolejności malejącej
+        max_time = 10
+        for i in range(0, tasks_val):
+            if(max_time < tab[i]):
+                max_time = tab[i]
+                place = i
+        tab[place] = 1
+        tab1[iter] = place
+        iter = iter + 1
+    #print(tab1)
+    return tab1
+
+def insert(sequence, position, value):
+    new_seq = sequence[:]
+    new_seq.insert(position, value)
+    return new_seq
+
+def neh(tasks, machines_val, tasks_val):
+    order = sum_and_order(tasks_val, machines_val, tasks)
+    current_seq = [order[0]]
+    for i in range(1, tasks_val):
+        min_cmax = float("inf")
+        for j in range(0, i + 1):
+            tmp = insert(current_seq, j, order[i])
+            cmax_tmp = makespan(tmp, tasks, machines_val)
+            #print(tmp, cmax_tmp)
+            if min_cmax > cmax_tmp:
+                best_seq = tmp
+                min_cmax = cmax_tmp
+        current_seq = best_seq
+    return current_seq, makespan(current_seq, tasks, machines_val)
+
+seq, cmax = neh(tasks, machines_val, tasks_val)
+print("Najlepsza sekwencja: ", seq)
+print("Najlepszy Cmax: ", cmax)
