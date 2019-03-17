0d773a9e82a6bc81f1436ad628755a068ae8689c
Kamil Goś <kamil.gos00@gmail.com>
2019-03-18 00:23:00 +0100

    Kosmetyczne zmiany

--- Zajecia_drugie/LAB2.py
+++ Zajecia_drugie/LAB2.py
@@ -1,8 +1,9 @@
-print("Welcome to LAB2")
+print("Welcome to neh algorithm (main version)")
+print("Starting algorithm... ")
 from Zajecia_drugie import read_data, neh
 
-tasks_val, machines_val, tasks = read_data.read_data("Zajecia_drugie/ta001.txt")
+tasks_val, machines_val, tasks = read_data.read_data("ta001.txt")
 
 seq, cmax = neh.neh(tasks, machines_val, tasks_val)
-print("Najlepsza sekwencja: ", seq)
-print("Najlepszy Cmax: ", cmax)
+print("Best sequence: ", seq)
+print("Best Cmax: ", cmax)
