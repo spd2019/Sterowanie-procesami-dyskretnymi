45a621c52c0afb6f14a6ad7db538fd4191ec90e2
Kamil Goś <kamil.gos00@gmail.com>
2019-04-01 10:44:35 +0200

    Akceleracja = gotowe Modyfikacja 1 = gotowe

--- Zajecia_drugie/LAB2.py
+++ Zajecia_drugie/LAB2.py
@@ -2,9 +2,13 @@ print("Welcome to neh algorithm (main version)")
 print("Starting algorithm... ")
 import read_data, neh
 import time
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.field_names = ["Algorithm", "Cmax", "Real time"]
 
 #MODIF?
-MODIF = 1
+MODIF = 4
 
 start = 0
 stop =0
@@ -12,24 +16,77 @@ stop =0
 tasks_val, machines_val, tasks = read_data.read_data("ta001.txt")
 
 
+if MODIF == 0:
+    print("Clasic neh")
+    start = time.perf_counter()
+    seq, cmax = neh.neh(tasks, machines_val, tasks_val)
+    stop = time.perf_counter()
+    print("Best sequence: ", seq)
+    print("Best Cmax: ", cmax)
+    policzony = round((stop-start), 3)
+    print("Time: ", policzony)
+
 if MODIF == 1:
     #mod
-    print("nowy neh")
-    start = time.clock()
+    print("MOD neh")
+    start = time.perf_counter()
     seq, cmax = neh.neh_wm(tasks, machines_val, tasks_val)
-    stop = time.clock()
+    stop = time.perf_counter()
+    print("Best sequence: ", seq)
+    print("Best Cmax: ", cmax)
+    policzony = round((stop-start), 3)
+    print("Time: ", policzony)
 
-if MODIF == 0:
-#Clasic neh
-    start = time.clock()
+if MODIF == 2:
+    print("Acc neh")
+    start = time.perf_counter()
+    seq, cmax = neh.neh_acc(tasks, machines_val, tasks_val)
+    stop = time.perf_counter()
+    print("Best sequence: ", seq)
+    print("Best Cmax: ", cmax)
+    policzony = round((stop-start), 3)
+    print("Time: ", policzony)
+
+
+
+
+
+if MODIF == 3: #porownanie czasow
+
+    print("Main neh:")
+    start = time.perf_counter()
     seq, cmax = neh.neh(tasks, machines_val, tasks_val)
-    stop = time.clock()
+    stop = time.perf_counter()
+    policzony = round((stop-start), 3)
+    print("Time: ", policzony)
 
+    table.add_row(["Classic", cmax, policzony ])
 
+    print("Acc:")
+    start = time.perf_counter()
+    seq, cmax = neh.neh_acc(tasks, machines_val, tasks_val)
+    stop = time.perf_counter()
+    policzony = round((stop - start), 3)
+    table.add_row(["Acc", cmax, policzony ])
 
-print("Best sequence: ", seq)
-print("Best Cmax: ", cmax)
-policzony = (stop-start)
-print("Time: ", policzony)
+    print(table)
 
-#Accelerated neh
+if MODIF == 4: #porownanie z mod
+
+    print("Acc neh:")
+    start = time.perf_counter()
+    seq, cmax = neh.neh_acc(tasks, machines_val, tasks_val)
+    stop = time.perf_counter()
+    policzony = round((stop-start), 3)
+    print("Time: ", policzony)
+
+    table.add_row(["Acc", cmax, policzony ])
+
+    print("Acc witch mod:")
+    start = time.perf_counter()
+    seq, cmax = neh.neh_wm(tasks, machines_val, tasks_val)
+    stop = time.perf_counter()
+    policzony = round((stop - start), 3)
+    table.add_row(["Acc WM", cmax, policzony ])
+
+    print(table)
--- Zajecia_drugie/neh.py
+++ Zajecia_drugie/neh.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+######### KLASYCZNY ###############################################
 def makespan(order, tasks, machines_val):
     times = []
     for i in range(0, machines_val):
@@ -26,7 +27,7 @@ def sum_and_order(tasks_val, machines_val, tasks):
     place = 0
     iter = 0
     while(iter != tasks_val):           #układa zadania w kolejności malejącej
-        max_time = 10
+        max_time = 0
         for i in range(0, tasks_val):
             if(max_time < tab[i]):
                 max_time = tab[i]
@@ -56,32 +57,10 @@ def neh(tasks, machines_val, tasks_val):
                 min_cmax = cmax_tmp
         current_seq = best_seq
 
-   # neh_acc(tasks, machines_val, tasks_val)
     return current_seq, makespan(current_seq, tasks, machines_val)
 
 
-
-def neh_wm(tasks, machines_val, tasks_val):
-    order = sum_and_order(tasks_val, machines_val, tasks)  # wagi w kolejnosci nierosnocej
-    current_seq = [order[0]]
-    for i in range(1, tasks_val):
-        min_cmax = float("inf")
-        for j in range(0, i + 1):
-            tmp = insert(current_seq, j, order[i])
-            #print("i", i)
-            graf, graf_wag, cmax_tmp, sciezka = create_graph_LR(tmp, tasks, machines_val)
-            cmax_tmp = makespan(tmp, tasks, machines_val)
-            if min_cmax > cmax_tmp:
-                best_seq = tmp
-                min_cmax = cmax_tmp
-                bestj = j
-        current_seq = best_seq
-        #print("bestj:", bestj)
-        current_seq = IR1_mod(sciezka, graf, bestj, current_seq, tasks, machines_val)
-       # print("sq:", current_seq)
-    print("koniec nowy neh")
-    return current_seq, makespan(current_seq, tasks, machines_val)
-
+################AKCELERACJA ALGORTYTMU NEH #########################################3
 
 def create_graph_LR(seq, tasks, machines_val):
     tasks_val = len(seq)  #ilosc zadan
@@ -92,9 +71,6 @@ def create_graph_LR(seq, tasks, machines_val):
     graf_wag = []
     for j in range(0, tasks_val):
         graf_wag.append([0]*machines_val)
-
-    #print("graf:", graf)
-   # print("new:", graf_wag)
 
     for i in range(0, len(graf)):
         if i == 0:
@@ -113,6 +89,7 @@ def create_graph_LR(seq, tasks, machines_val):
 
     Cmax = graf_wag[-1][-1]
 
+ #wyznaczanie najdłuższej ścieżki
     i = 0;
     j = 0;
     sciezka = []
@@ -126,14 +103,11 @@ def create_graph_LR(seq, tasks, machines_val):
             j = j+1
     sciezka.append([0, 0])
 
-    #print("sciezka:", sciezka)
+    return sciezka, graf, graf_wag
 
-    #print("gw:",graf_wag)
-    return graf, graf_wag, Cmax, sciezka
 
 
 def create_graph_RL(seq, tasks, machines_val):
-    print("ODWROTNY")
     tasks_val = len(seq)  #ilosc zadan
     graf = []
     for i in seq:
@@ -143,14 +117,12 @@ def create_graph_RL(seq, tasks, machines_val):
     for j in range(0, tasks_val):
         graf_wag.append([0]*machines_val)
 
-    #print("graf:", graf)
-    #print("new:", graf_wag)
-
     for i in range(1, len(graf)+1):
         if i == 0:
             for j in range(1, machines_val+1):
                 if j==0:
-                    graf_wag[-i][-j] = graf[-i][-j]
+                    graf_wag[-i][-j] = graf[-
+                    i][-j]
                 else:
                     graf_wag[-i][-j] = graf[-i][-j] + graf_wag[-i][-j+1]
 
@@ -162,77 +134,108 @@ def create_graph_RL(seq, tasks, machines_val):
                    graf_wag[-i][-j] = graf[-i][-j] + max( graf_wag[-i+1][-j], graf_wag[-i][-j+1]) #lewo i gora
 
     Cmax = graf_wag[0][0]
-    #print("gw:",graf_wag)
-    return graf_wag, Cmax
+    return graf_wag
 
 
-def IR1_mod(sciezka, graf, bestj , seq, tasks, machines_val):
-    #print(seq)
-    #print("AAA",sciezka)
-    #print(graf)
-    najnowsze = 999
-    for k in range(len(seq)):
-        if bestj == seq[k]:
-            najnowsze = k
-    max_task = 0
-    max_task_wsp = []
-    for i in sciezka:
-        if i[0] != najnowsze:
-            #print(i)
-            #print(graf[i[0]][i[1]])
-            if max_task < graf[i[0]][i[1]]:
-                max_task = graf[i[0]][i[1]]
-                max_task_wsp = i
-
-    seq_idx = seq[max_task_wsp[0]]
-    #print("seq_idx",seq_idx)
-    #print(seq)
-    seq.remove(seq_idx)
-    #print("seqar", seq)
-    new_mincmax = 999
-    for i in range(0, len(seq)+1):
-        tmp = insert(seq, i, seq_idx)
-        #print(i)
-       # print(tmp)
-        newcmax_tmp = makespan(tmp, tasks, machines_val)
-        if new_mincmax > newcmax_tmp:
-            new_seq = tmp
-            new_mincmax = newcmax_tmp
-    #print("newseq",new_seq)
-    #print("new_min_cmax", new_mincmax)
-    return new_seq
-
-
-
-def neh_akceleracja(kolejnosc, obciazenie1, obciazenie2, tabela, element):
+def makespan_acc(order, weight_LR, weight_RL, tasks, new_elem):
     Cmax = []
-    for i in range(len(obciazenie2) + 1):
+    for i in range(len(weight_RL) + 1):
         temp = []
         C = []
-        if i == 0:
-            for j in range(len(tabela[element])):
+        if i == 0: #wstawiam element na początek uszeregowania
+            for j in range(len(tasks[new_elem])):
                 if j == 0:
-                    temp.append(tabela[element][j])
+                    temp.append(tasks[new_elem][j])
                 else:
-                    temp.append(tabela[element][j] + temp[j - 1])
-                C.append(temp[j] + obciazenie2[i][j])
+                    temp.append(tasks[new_elem][j] + temp[j - 1])
+                C.append(temp[j] + weight_RL[i][j])
             Cmax.append(max(C))
-        elif i == len(obciazenie2):
-            for j in range(len(tabela[element])):
+        elif i == len(weight_RL): #wstawiam element na koniec
+            for j in range(len(tasks[new_elem])):
                 if j == 0:
-                    temp.append(tabela[element][j] + obciazenie1[i - 1][j])
+                    temp.append(tasks[new_elem][j] + weight_LR[i - 1][j])
                 else:
-                    temp.append(tabela[element][j] + max(obciazenie1[i - 1][j], temp[j - 1]))
+                    temp.append(tasks[new_elem][j] + max(weight_LR[i - 1][j], temp[j - 1]))
                 C.append(temp[j])
             Cmax.append(max(C))
-        else:
-            for j in range(len(tabela[element])):
+        else: #wstawiam element w środek
+            for j in range(len(tasks[new_elem])):
                 if j == 0:
-                    temp.append(tabela[element][j] + obciazenie1[i - 1][j])
+                    temp.append(tasks[new_elem][j] + weight_LR[i - 1][j])
                 else:
-                    temp.append(tabela[element][j] + max(obciazenie1[i - 1][j], temp[j - 1]))
-                C.append(temp[j] + obciazenie2[i][j])
+                    temp.append(tasks[new_elem][j] + max(weight_LR[i - 1][j], temp[j - 1]))
+                C.append(temp[j] + weight_RL[i][j])
             Cmax.append(max(C))
     miejsce = Cmax.index(min(Cmax))
-    kolejnosc.insert(miejsce, element)
-    return kolejnosc, min(Cmax)
+    order.insert(miejsce, new_elem)
+    return order, min(Cmax)
+
+
+
+
+def neh_acc(tasks, machines_val, tasks_val):
+    order = sum_and_order(tasks_val, machines_val, tasks)
+    k=0;
+    print("order:", order)
+    for i in order:
+        if k == 0:
+            current_seq = [order[0]]
+            path, graph, weight_LR = create_graph_LR(current_seq, tasks, machines_val)
+            weight_RL = create_graph_RL(current_seq, tasks, machines_val)
+            k+=1
+        else:
+            path, graph, weight_LR = create_graph_LR(current_seq, tasks, machines_val)
+            weight_RL = create_graph_RL(current_seq, tasks, machines_val)
+            current_seq, Cmax = makespan_acc(current_seq, weight_LR, weight_RL, tasks, i)
+
+    print("Acc_seq:", current_seq)
+    print("Acc_cmax: ", Cmax)
+    return current_seq, Cmax
+
+
+#################### MODYFIKACJA ###################################
+
+def neh_wm(tasks, machines_val, tasks_val):
+    order = sum_and_order(tasks_val, machines_val, tasks)
+    k = 0;
+    print("order:", order)
+    for i in order:
+        if k == 0:
+            current_seq = [order[0]]
+            path, graph, weight_LR = create_graph_LR(current_seq, tasks, machines_val)
+            weight_RL = create_graph_RL(current_seq, tasks, machines_val)
+            k += 1
+        else:
+            path, graph, weight_LR = create_graph_LR(current_seq, tasks, machines_val)
+            weight_RL = create_graph_RL(current_seq, tasks, machines_val)
+            current_seq, Cmax = makespan_acc(current_seq, weight_LR, weight_RL, tasks, i)
+
+            MOD = NEH_IR1(path, graph, i , current_seq)
+            if MOD!= -1:
+                print(MOD)
+                current_seq.remove(MOD)
+                path, graph, weight_LR = create_graph_LR(current_seq, tasks, machines_val)
+                weight_RL = create_graph_RL(current_seq, tasks, machines_val)
+                current_seq, Cmax = makespan_acc(current_seq, weight_LR, weight_RL, tasks, MOD)
+
+    print("Acc_seq:", current_seq)
+    print("Acc_cmax: ", Cmax)
+    return current_seq, Cmax
+
+
+
+def NEH_IR1(sciezka, graf, i, kolejnosc):
+    max = 0;
+    index = 999;
+    zadanie_odrzucone = 999
+    for k in range(len(kolejnosc)):
+        if i == kolejnosc[k]:
+            zadanie_odrzucone = k
+    for i in sciezka:
+        if i[0] != zadanie_odrzucone:
+            if graf[i[0]][i[1]] > max:
+                max = graf[i[0]][i[1]]
+                index = i[0]
+    if index == 999:
+        return -1
+    return kolejnosc[index]
