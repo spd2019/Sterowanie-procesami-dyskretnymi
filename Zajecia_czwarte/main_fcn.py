934b75efaa97ebdc2c6a9390cfae4c53ac10512e
Kamil Goś <kamil.gos00@gmail.com>
2019-05-04 16:12:47 +0200

    main function added (witch result compare)

--- Zajecia_czwarte/main_fcn.py
+++ Zajecia_czwarte/main_fcn.py
@@ -2,12 +2,13 @@ import numpy as np
 from Zajecia_czwarte import read_data as rd
 from Zajecia_czwarte import schrage
 
-print("Welcome to Schrage algorithm")
-print("----------------------------")
-print("Which test would you like to use?")
-print(" 1. in50 \r\n 2. in100 \r\n 3. in200")
-test_nr = int(input("Enter your choose: "))
+# print("Welcome to Schrage algorithm")
+# print("----------------------------")
+# print("Which test would you like to use?")
+# print(" 1. in50 \r\n 2. in100 \r\n 3. in200")
+# test_nr = int(input("Enter your choose: "))
 
+test_nr = 1
 
 if test_nr == 1:
     tasks_val, columns_val, tasks = rd.read_data("in50.txt")
@@ -29,3 +30,4 @@ elif test_nr == 3:
     print("Correct result for in200 test is 6416")
     print("Your result is: ",cmax)
     if cmax == 6416: print("CORRECT!")
+
--- Zajecia_czwarte/schrage.py
+++ Zajecia_czwarte/schrage.py
@@ -1,42 +1,41 @@
 import numpy as np
+import copy
 
 def Schrage(tasks_val, tasks):
     print("Starting Schrage algorithm")
-    S = []
-    for i in range(0, tasks_val):
-        S.append(tasks[i][0])
-    delta = []
-    Ng = []
-    Nn = []
-    Q = []
-    for i in range(0, tasks_val):
-        Nn.append(i)
-    t = 0
-    k = 1
-    cmax = 0
-    e = 0
 
-    while Ng != [] or Nn != []:
-        while Nn != [] and min(S) <= t:
-            e = np.argmin(S)
-            Ng.append(e)
-            print("Ng: ", Ng)
-            Nn.remove(e)
-            print("Nn: ", Nn)
-            S[e]=50
-            print("S: ", S)
+
+    sigma = []   #czesciowa kolejnosc skladajaca sie z uszeregowanych zadan
+    Ng = []  # zadania gotowe do uszeregowania
+    Nn = copy.deepcopy(tasks)# zadania nieuszeregowane
+    R = copy.deepcopy(Nn[:,0])
+    t = min(Nn[:,0])  # minimalny czas przygotowania
+    print(t)
+    i = 1
+
+
+    while ( Ng != [] or Nn != [] ):
+        while ( Nn != []  and  min(Nn[:,0]) <= t):
+            Qnn = copy.deepcopy(Nn[:,0])
+            j = np.argmin(Qnn)
+            print("j = ",j)
+
+            Ng.append(Nn[j,:])
+            Nn = np.delete(Nn, (j), axis=0)
+            #print("Ng: ", Ng)
+            #print("Nn: ", Nn)
+            #print("S: ", S)
         if Ng == []:
-            t = min(S)
+            t = min(Nn[:,0])
         else:
-            for i in Ng:
-                Q.append(tasks[i][2])
-            e = np.argmax(Q)
-            if e in Ng:
-                Ng.remove(e)
-            delta.append(e)
-            k = k+1
-            t = t+tasks[e][1]
-            cmax = max(cmax, t+tasks[e][2])
-    print("delta: ", delta)
+
+            j = np.argmax(Ng[:,0])
+            Ng = np.delete(Ng, (j), axis=0)
+            sigma[i] = j
+            print("sigma = ", sigma)
+            i = i+1
+            t = t+tasks[i][1]
+            cmax = max(cmax, t+tasks[j][2])
+    print("sigma: ", sigma)
     print("Cmax: ", cmax)
     return cmax
