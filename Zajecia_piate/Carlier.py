ca0a10c28af09c025962c46a20bf57e79737e841
spd2019 <47981399+spd2019@users.noreply.github.com>
2019-05-20 09:10:24 +0200

    Update Carlier.py

--- Zajecia_piate/Carlier.py
+++ Zajecia_piate/Carlier.py
@@ -1,37 +1,30 @@
-def get_b(U, pi, tab):
+def get_b(U, pi, tasks_val):
     p = 0
-    for i in pi:
-        r = tab[i][0]
-        p = max(p, r) + tab[i][1]
-        if U == p + tab[i][2]:
+    for i in range(0, tasks_val):
+        r = pi[i][0]
+        p = max(p, r) + pi[i][1]
+        if U == p + pi[i][2]:
             j = i
     return j
 
 
-def get_a(U, pi, b, tab):
-    q = tab[b][2]
-    mx = pi.index(b)
-    for i in pi:
+def get_a(U, pi, b, tasks_val):
+    q = pi[b][2]
+    for i in range(0, tasks_val):
         p = 0
-        r = tab[i][0]
-        mi = pi.index(i)
-        for a in range(mi, mx+1):
-            p += tab[pi[a]][1]
+        r = pi[i][0]
+        for a in range(i, b+1):
+            p += pi[a][1]
         if U == r+p+q:
             return i
 
-
-def get_c(pi,a,b,tab):
+def get_c(pi, a, b):
     flaga = 0
-    a = pi.index(a)
-    b = pi.index(b)
     for i in range(a, b+1):
-        if tab[pi[i]][2] < tab[pi[b]][2]:
-            j = pi[i]
+        if pi[i][2] < pi[b][2]:
+            j = i
             flaga = 1
     if flaga == 1:
         return j
     else:
         return []
-
-
