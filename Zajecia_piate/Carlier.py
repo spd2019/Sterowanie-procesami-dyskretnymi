013b63acc91a3c1bf8d2cdf6631d2c714f82a0a8
spd2019 <47981399+spd2019@users.noreply.github.com>
2019-06-02 18:52:41 +0200

    Update Carlier.py

--- Zajecia_piate/Carlier.py
+++ Zajecia_piate/Carlier.py
@@ -1,3 +1,88 @@
+import operator
+
+def read_data_2list(filename):
+    file = open(filename, "r")
+    tasks_val, columns_val = file.readline().split()
+    tasks_val = int(tasks_val)
+    columns_val = int(columns_val)
+    tasks = []
+    values = []
+    for val in file.read().split():
+        values.append(float(val))
+
+    for a in range(0, len(values), 3):
+        tmp_tab = []
+        for b in range(0,3):
+            tmp_tab = tmp_tab + [values[a+b]]
+        tasks.append(tmp_tab)
+
+
+    #print("Number of tasks: ", tasks_val)
+    #print("Number of columns: ", columns_val)
+    #print("Tasks: \n", tasks)
+    file.close()
+    return tasks_val, columns_val, tasks
+
+
+def Schrage(tasks):
+    # print("Starting Schrage algorithm")
+
+    sigma = []   #czesciowa kolejnosc skladajaca sie z uszeregowanych zadan
+    Ng = []  # zadania gotowe do uszeregowania
+    Nn = tasks.copy() # zadania nieuszeregowane
+    t = min(Nn)[0]  # minimalny czas przygotowania
+    Cmax = 0
+
+    while (Ng !=[] or Nn!=[]):
+        while(Nn !=[] and min(Nn)[0] <=t):
+            j = Nn.index(min(Nn))
+            Ng.append(Nn[j])
+            Nn.pop(j)
+        if Ng == []:
+            t = min(Nn)[0]
+        else:
+            j = Ng.index(max(Ng, key=operator.itemgetter(2)))
+            tmp = Ng[j]
+            Ng.pop(j)
+            sigma.append(tmp)
+            t = t + tmp[1]
+            Cmax = max(Cmax, t+tmp[2])
+    return  sigma, Cmax
+
+
+def Schrage_pmtn(tasks):
+    # print("Starting Schrage pmtn algorithm")
+
+    sigma = []  # czesciowa kolejnosc skladajaca sie z uszeregowanych zadan
+    Ng = []  # zadania gotowe do uszeregowania
+    Nn = tasks.copy()  # zadania nieuszeregowane
+    t = 0
+    Cmax = 0
+    l = [0, 0, 0]  #aktualnie wykonywane zadanie
+
+    while (Ng != [] or Nn != []):
+        while (Nn != [] and min(Nn)[0] <= t):
+            j = Nn.index(min(Nn))
+            tmp = Nn[j]
+            Ng.append(tmp)
+            Nn.pop(j)
+            if tmp[2] > l[2]:
+                l[1] = t-tmp[0]
+                t = tmp[0]
+                if l[1] > 0:
+                    Ng.append(l)
+        if Ng == []:
+             t = min(Nn)[0]
+        else:
+            i = Ng.index(max(Ng, key=operator.itemgetter(2)))
+            j = Ng[i]
+            Ng.pop(i)
+            sigma.append(j)
+            l = j
+            t = t + j[1]
+            Cmax = max(Cmax, t + j[2])
+    return sigma, Cmax
+
 def get_b(U, pi, tasks_val):
     p = 0
     for i in range(0, tasks_val):
@@ -6,7 +91,6 @@ def get_b(U, pi, tasks_val):
         if U == p + pi[i][2]:
             j = i
     return j
-
 
 def get_a(U, pi, b, tasks_val):
     q = pi[b][2]
@@ -18,7 +102,7 @@ def get_a(U, pi, b, tasks_val):
         if U == r+p+q:
             return i
 
-def get_c(pi, a, b):
+def get_c(pi,a,b):
     flaga = 0
     for i in range(a, b+1):
         if pi[i][2] < pi[b][2]:
@@ -29,17 +113,10 @@ def get_c(pi, a, b):
     else:
         return []
 
-def get_cmax(order):
-    t = 0
-    cm = 0
-    for i in range(0, tasks_val):
-        t = max(order[i][0], t) + order[i][1]
-        cm = max(cm, t + order[i][2])
-    return cm
-
 def Carlier(tasks, tasks_val):
     pi, U = Schrage(tasks)
-    UB = float("inf")
+    global UB
+    global pi_gw
     if U < UB:
         UB = U
         pi_gw = pi
@@ -47,7 +124,7 @@ def Carlier(tasks, tasks_val):
     a = get_a(U, pi, b, tasks_val)
     c = get_c(pi, a, b)
     if c == []:
-        return pi_gw
+        return
     K = []
     for i in range(c+1, b+1):
         K.append(i)
@@ -64,6 +141,8 @@ def Carlier(tasks, tasks_val):
     Kc = []
     for i in range(c, b+1):
         Kc.append(i)
+    remember_r = pi[c][0]
+    pi[c][0] = max(pi[c][0], Kr + Kp)
     Kcr = []
     Kcp = 0
     Kcq = []
@@ -74,8 +153,6 @@ def Carlier(tasks, tasks_val):
     Kcr = min(Kcr)
     Kcq = min(Kcq)
     Khc = Kcr + Kcp + Kcq
-    remember_r = pi[c][0]
-    pi[c][0] = max(pi[c][0], Kr + Kp)
     ord, LB = Schrage_pmtn(tasks)
     LB = max(Kh, Khc, LB)
     if LB < UB:
@@ -83,14 +160,27 @@ def Carlier(tasks, tasks_val):
     pi[c][0] = remember_r
     remember_q = pi[c][2]
     pi[c][2] = max(pi[c][2], Kq+Kp)
+    Kcr = []
+    Kcp = 0
+    Kcq = []
+    for j in Kc:
+        Kcr.append(pi[j][0])
+        Kcp += pi[j][1]
+        Kcq.append(pi[j][2])
+    Kcr = min(Kcr)
+    Kcq = min(Kcq)
+    Khc = Kcr + Kcp + Kcq
     ord, LB = Schrage_pmtn(tasks)
     LB = max(Kh, Khc, LB)
     if LB < UB:
         Carlier(tasks, tasks_val)
     pi[c][2] = remember_q
-    return pi_gw
 
-#order = Carlier(tasks, tasks_val)
-#cm = get_cmax(order)
-#print(order)
-#print(cm)
+data = [0,1,2,3,4,5,6,7,8]
+
+for i in data:
+    UB = 9999999
+    plik = "data" + str(i) + ".txt"
+    tasks_val, columns_val, tasks = read_data_2list(plik)
+    Carlier(tasks, tasks_val)
+    print("{} {}".format(plik, UB))
